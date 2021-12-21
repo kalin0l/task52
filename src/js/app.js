13e90fcc33ecb3696cb65223a47098e5df0c7597
kalin0l <aleksiev2494@gmail.com>
2021-12-21 19:23:37 +0200

    add

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,23 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  // const button = document.querySelector(".button");
+  // button.addEventListener("click", () => {
+  //   alert("💣");
+  // });
+  
+    const body = document.querySelector('body');
+    const article = document.querySelectorAll('article');
+    document.body.addEventListener('onClick',() => {
+     for(let i = 0;i <=5;i++){
+       const article = document.createElement("article");
+       article.classList.add('message');
+       document.body.appendChild(article);
+     }
+     article.forEach(item => {
+       item.textContent = 'msg';
+     })
+     
+      })
+    
 });
